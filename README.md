4beea542e181cda91c400bae31412f52ad8294b7
viooltje <chantal.houtschild@outlook.com>
2021-05-17 21:38:23 +0200

    Initial commit - js document with first function - html file with basic structure - README file with short description - ComputerPlay() is tested

---
+++ README.md
@@ -0,0 +1,2 @@
+Rock Paper Scissors program with JavaScript.
+The program will work from the command line. 
---
+++ rps.js
@@ -0,0 +1,12 @@
+// Returning rock, paper, scissors randomly 
+function computerPlay() {
+    let choices = ['Rock', 'Paper','Scissor'];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice; 
+}
+
+
+//
+//fucntion round(playerSelection, ComputerSelection) {
+
+//}
