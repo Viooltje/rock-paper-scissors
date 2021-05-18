2544e1ce59b5ac145c4e039845874e2bb60ed26f
viooltje <chantal.houtschild@outlook.com>
2021-05-18 19:58:09 +0200

    Added new functionto determine winner. The function is tested

--- rps.js
+++ rps.js
@@ -6,7 +6,28 @@ function computerPlay() {
 }
 
 
-//
-//fucntion round(playerSelection, ComputerSelection) {
+function determineWinner(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return `It's a draw, ${computerSelection} is the same as ${playerSelection}`;
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return `You won!{playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return `You lose ${playerSelection} is defeated ${computerSelection}`;
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return `You won!{playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == 'rock' && computerSelection == 'paper'){
+        return `You lose ${playerSelection} is defeated ${computerSelection}`;
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return `You lose ${playerSelection} is defeated ${computerSelection}`;
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return `You won ${playerSelection} beats ${computerSelection}`;
+    }
+}
 
-//}
+determineWinner('rock', 'paper');
+determineWinner('paper', 'rock');
+determineWinner('scissors', 'rock')
+determineWinner('scissors', 'scissors');
+determineWinner('scissors', 'paper' );
