d23127b498e0081e63adffe3ec546242deb5d7ff
viooltje <chantal.houtschild@outlook.com>
2021-05-28 20:44:42 +0200

    Finished the rps program added a new function to determine end winner after 5 rounds Added lines that will use the functions There is still a bug somewhere, console log is not giving me clearity now, it shows to console.logs, so it's not clear if it workds alright or not Need to have a function that determens the end winner now it's a console.log statment.

--- rps.js
+++ rps.js
@@ -5,29 +5,60 @@ function computerPlay() {
     return computerChoice; 
 }
 
-
-function determineWinner(playerSelection, computerSelection) {
+// Determine winner of 1 round
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
-        return `It's a draw, ${computerSelection} is the same as ${playerSelection}`;
+        console.log(`It's a draw ${computerSelection} ${playerSelection}`);
+        return `It's a draw`;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return `You won!{playerSelection} beats ${computerSelection}`;
+        console.log(`You won! ${computerSelection} ${playerSelection}`);
+        return `You won!`;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return `You lose ${playerSelection} is defeated ${computerSelection}`;
+        console.log(`You lose ${computerSelection} ${playerSelection}`);
+        return `You lose`;
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return `You won!{playerSelection} beats ${computerSelection}`;
+        console.log(`You won! ${computerSelection} ${playerSelection}`);
+        return `You won!`;
     } else if (playerSelection == 'rock' && computerSelection == 'paper'){
-        return `You lose ${playerSelection} is defeated ${computerSelection}`;
+        console.log(`You lose ${computerSelection} ${playerSelection}`);
+        return `You lose`;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return `You lose ${playerSelection} is defeated ${computerSelection}`;
+        console.log(`You lose ${computerSelection} ${playerSelection}`);
+        return `You lose`;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `You won ${playerSelection} beats ${computerSelection}`;
+        console.log(`You won! ${computerSelection} ${playerSelection}`);
+        return `You won`;
     }
 }
 
-determineWinner('rock', 'paper');
-determineWinner('paper', 'rock');
-determineWinner('scissors', 'rock')
-determineWinner('scissors', 'scissors');
-determineWinner('scissors', 'paper' );
+// Determine the end winner
+function endWinner(playerScore, computerScore){
+    if (playerScore == computerScore) {
+        return `It's a draw your end score is ${playerScore} your opponents is ${computerScore}`;
+    } else if (playerScore > computerScore) {
+        return `You won! your end score is ${playerScore} your opponents score is ${computerScore}`
+    } else {
+        return  `You lose your end score is ${playerScore} your opponents score is ${computerScore} `
+    }
+}
+
+// variables for the program
+let computerScore = 0;
+let playerScore= 0;
+
+// Start of game
+for(let i=0; i<5; i++) {
+    const playerSelection = prompt("Type rock paper or scissors");
+    const computerSelection =computerPlay();
+    if (playRound(playerSelection, computerSelection) == 'You won!') {
+        playerScore++;
+    } else if (playRound(playerSelection, computerSelection) == 'You lose') {
+        computerScore++;
+    }    
+}
+console.log(endWinner(playerScore, computerScore));
+
+
+
